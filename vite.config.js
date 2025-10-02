72fb154563d04aefd84dd712170b354a80406625
Joshua Johnston <your.email@example.com>
2025-10-02 13:20:43 -0400

    Remove Tailwind config from Vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // GitHub Pages deployment base path
-  base: '/todo-reacts/',
-
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
-
+  base: '/todo-reacts/',  // ✅ important for GitHub Pages
   build: {
     sourcemap: true,
   },
